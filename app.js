cac05b46e9a473e3a1c8e2bd554e377f9b6ec7f3
Will <wk1394@gmail.com>
2020-06-02 16:29:52 -0500

    shapes now print their data when clicked and delete themselves when double clicked

--- app.js
+++ app.js
@@ -5,14 +5,20 @@ let triangleBtn = document.getElementById("triangleBtn");
 let shapeDiv = document.getElementById("shapeDiv");
 const MAX = 570;
 
+
 class Shape {
   constructor(type, width, height) {
     this.type = type;
     this.width = width;
     this.height = height;
+    this.radius = "-"
+    this.perimeter = (this.height * 2) + (this.width * 2);
+    this.area = this.height * this.width;
     this.reduceToMax();
+    this.draw();
+
   }
-  
+
   draw() {
     this.div = document.createElement("div");
     this.div.classList.add(this.type);
@@ -21,58 +27,91 @@ class Shape {
     let maxHeight = document.getElementById("shapeDiv").offsetHeight - this.height;
     let maxWidth = document.getElementById("shapeDiv").offsetWidth - this.width;
     shapeDiv.appendChild(this.div);
-    this.div.style.left = `${((Math.random() * maxWidth))}px`;
-    this.div.style.top = `${(Math.random() * maxHeight)}px`;
+    this.div.style.left = `${Math.random() * maxWidth}px`;
+    this.div.style.top = `${Math.random() * maxHeight}px`;
+    this.div.addEventListener("click", () => this.getInfo());
+    this.div.addEventListener("dblclick", () => this.remove());
   }
 
   reduceToMax() {
-    if(this.width || this.height == MAX) {
+    if (this.width >= MAX) {
       this.width = MAX;
+    }
+    if(this.height >= MAX) {
       this.height = MAX;
     }
+  }
+    
+  
+
+  getInfo() {
+    let infoDiv = document.getElementById("infoDiv");
+    infoDiv.innerHTML =
+     `Shape Info: <br> 
+     Width: ${this.width} <br>
+     Height: ${this.height} <br>
+     Radius: ${this.radius} <br>
+     Area: ${this.area} <br>
+     Perimeter: ${this.perimeter}`
+
+  }
+
+  remove() {
+    event.target.remove();
   }
 
 }
 
 class Rectangle extends Shape {
-  constructor(type, width = 0, height = 0) {
+  constructor(type, width, height) {
     super(type, width, height);
-    this.draw();
   }
 }
 
+//This takes in an extra parameter so that the original draw function will work properly, width and height are identical
 class Square extends Rectangle {
   constructor(type, width, height) {
     super(type, width, height);
   }
 }
 
+//This works like the square, again so the draw function will work. Width and height are identical
 class Circle extends Shape {
   constructor(type, width, height) {
     super(type, width, height);
-    this.draw();
+    this.radius = width / 2;
+    this.perimeter = Math.round(2 * Math.PI * this.radius);
+    this.area = Math.round(Math.PI * (this.radius * this.radius));
   }
+
 }
 
+//This takes a width parameter, but frankly doesn't need it and I don't know how to call the super and only take the parameters I need.
+//This has its own draw function because it uses different css stylings than the other shapes
 class Triangle extends Shape {
   constructor(type, width, height) {
     super(type, width, height);
-    this.draw();
+    this.perimeter = 2 * this.height + (Math.sqrt(2)) * this.height;
+    this.area = 0.5 * this.height * this.height;
   }
 
   draw() {
     this.div = document.createElement("div");
     this.div.classList.add(this.type);
-    this.div.style.borderBottomWidth = `${this.width}px`;
+    this.div.style.borderBottomWidth = `${this.height}px`;
     this.div.style.borderRightWidth = `${this.height}px`;
-    this.div.style.borderLeftWidth = `${this.height}px`;
     let maxHeight = document.getElementById("shapeDiv").offsetHeight - this.height;
-    let maxWidth = document.getElementById("shapeDiv").offsetWidth - this.width;
+    let maxWidth = document.getElementById("shapeDiv").offsetWidth - this.height;
     shapeDiv.appendChild(this.div);
-    this.div.style.left = `${((Math.random() * maxWidth))}px`;
-    this.div.style.top = `${(Math.random() * maxHeight)}px`;
+    this.div.style.left = `${Math.random() * maxWidth}px`;
+    this.div.style.top = `${Math.random() * maxHeight}px`;
+    this.div.addEventListener("click", () => this.getInfo());
+    this.div.addEventListener("dblclick", () => this.remove());
   }
 }
+
+
+//Listeners for each button
 
 squareBtn.addEventListener("click", () => {
   new Square(
@@ -102,14 +141,6 @@ triangleBtn.addEventListener("click", () => {
   new Triangle(
     "triangle",
     document.querySelector("#triangleHeight").value,
-    document.querySelector("#triangleHeight").value / 2
+    document.querySelector("#triangleHeight").value
   );
 });
-
-function getRandomX() {
-  return Math.floor(Math.random() * maxWidth);
-}
-
-function getRandomY() {
-  return Math.floor(Math.random() * maxHeight);
-}
